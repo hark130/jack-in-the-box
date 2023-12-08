c854464131219d33e9bb608d65aade43b272fe38
hark130 <hark130@yahoo.com>
2023-12-08 16:02:25 -0600

    dev: JITB initial commit
    Now it's a Python package that executes locally.
    
    Changes to be committed:
    	new file:   jitb/__init__.py
    	new file:   jitb/__main__.py
    	new file:   jitb/jitb_main.py

---
+++ jitb/__main__.py
@@ -0,0 +1,6 @@
+from jitb.jitb_main import main
+import sys
+
+
+if __name__ == '__main__':
+	sys.exit(main())
---
+++ jitb/jitb_main.py
@@ -0,0 +1,17 @@
+"""Defines the entry-point function for this package."""
+
+
+def main() -> int:
+    """Entry point function.
+
+    Returns:
+        0 on success, 1 for failure.
+    """
+    # LOCAL VARIABLES
+    exit_code = 0  # 0 for success, 1 for failure.
+
+    # DO IT
+    pass
+
+    # DONE
+    return exit_code
