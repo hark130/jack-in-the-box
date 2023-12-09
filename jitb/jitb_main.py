915d7ba3458dd51793002558846baca51e660ada
hark130 <hark130@yahoo.com>
2023-12-09 16:42:49 -0600

    dev: Big main() updated
    This is the start of a series of huge commits.
    Basically, I've been sitting on this code base until it was mostly
    functional.
    Now, not only will it survive all the way through Thriplash, it will
    also respond appropriately when a new game with started with the same
    players.
    
    As of right now, JITB doesn't actually *answer* the Thriplash, but this
    is close enough...
    At least close enough to commit these changes.
    
    Changes to be committed:
    	modified:   jitb/jitb_main.py
    
    Untracked files:
    	jitb/jitb_args.py
    	jitb/jitb_globals.py
    	jitb/jitb_website.py

--- jitb/jitb_main.py
+++ jitb/jitb_main.py
@@ -1,4 +1,12 @@
 """Defines the entry-point function for this package."""
+# Standard
+import sys
+# Third Party
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+import selenium
+# Local
+from jitb.jitb_args import parse_args
+from jitb.jitb_website import answer_prompts, join_room, play_the_game, select_character, vote_answers
 
 
 def main() -> int:
@@ -8,10 +16,37 @@ def main() -> int:
         0 on success, 1 for failure.
     """
     # LOCAL VARIABLES
-    exit_code = 0  # 0 for success, 1 for failure.
+    exit_code = 0   # 0 for success, 1 for failure.
+    room_code = ''  # Jackbox Games room code
+    username = ''   # Jackbox Games username
+    # driver = None   # Webdriver for the Jackbox Games website
 
     # DO IT
-    pass
+    (room_code, username) = parse_args()
+    # print(f'ROOM CODE: {room_code}')  # DEBUGGING
+    # print(f'USERNAME: {username}')  # DEBUGGING
+    try:
+        play_the_game(room_code=room_code, username=username)
+        # driver = join_room(room_code=room_code, username=username)
+        # select_character(driver)
+        # # TO DO: DON'T DO NOW... WAIT FOR GAME TO START (by waiting for char selection to go away)
+        # answer_prompts(driver)
+        # vote_answers(driver)
+        # input()  # DEBUGGING
+        # print(f'POST-INPUT PAGE SOURCE:\n{driver.page_source}')  # DEBUGGING
+    except (KeyboardInterrupt, NoSuchElementException, RuntimeError, TimeoutException) as err:
+        # if driver:
+        #     print(f'SOURCE: {driver.page_source}')  # DEBUGGING
+        _print_exception(err)
+        exit_code = 1
 
     # DONE
+    # input()  # DEBUGGING
+    # if driver:
+    #     driver.close()
     return exit_code
+
+
+def _print_exception(error: Exception) -> None:
+    """Print an exception message to stderr."""
+    print(repr(error), file=sys.stderr)
