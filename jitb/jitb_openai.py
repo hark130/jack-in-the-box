3b42b77f632f149aee1cd08808221b3baffc4a7e
hark130 <hark130@yahoo.com>
2023-12-10 14:30:06 -0600

    JITB-1: Added JitbAi fault tolerance
    OpenAI doesn't always follow instructions.
    E.g.
    	- I say select a choice and it generates new content.
    	- I say just give me a letter but it provides additional context
    	- There are policies/guidelines against explicit or inappropriate language
    
    I added a lot of jitb_openai functionality to detect failed requests and
    extrapolate OpenAI's answer when it doesn't exactly follow instructions.
    
    TO DO: DON'T DO NOW...
    	- Enforce answer length limit
    	- Add context for fill-in-the-blank questions
    	- Integrate Thriplash answer generation
    	- Clean up Thriplash button text for voting
    
    Changes to be committed:
    	modified:   jitb/jitb_openai.py

--- jitb/jitb_openai.py
+++ jitb/jitb_openai.py
@@ -1,6 +1,10 @@
 """The package's interface to OpenAI's API."""
 # Standard
+from collections import OrderedDict
 import os
+import re
+import random
+import string
 # Third Party
 from openai import OpenAI
 # Local
@@ -31,6 +35,16 @@ class JitbAi:
             {'role': 'system',
              'content': 'You are a funny person playing Jackbox Games Quiplash 3.'},
             # {'role': 'user', 'content': 'Tell me a funny joke.'}
+        ]
+        # Content message indications the OpenAI did not answer a query
+        self._failure_content = [
+            "As an AI language model, I am committed to maintaining a respectful and " \
+            + "inclusive environment. I cannot endorse or participate in generating content " \
+            + "that is offensive, explicit, or inappropriate. If you have any other prompt or " \
+            + "topic you'd like me to help with, I'd be more than happy to assist you.",
+            "I can't comply with this request.",
+            "I'm unable to assist with that request.",
+            "I'm sorry, I can't assist with that.",
         ]
 
     def setup(self) -> None:
@@ -70,6 +84,7 @@ class JitbAi:
         # GENERATE IT
         messages.append({'role': 'user', 'content': content})
         answer = self._create_content(messages=messages)
+        answer = re.sub(r'^"|"$', '', answer)  # Strip leading and trailing quotes
 
         # DONE
         return answer
@@ -85,12 +100,34 @@ class JitbAi:
             One of the answers entries.
         """
         # LOCAL VARIABLES
-        favorite = ''  # Favorite from the answers list
+        favorite = ''                   # Favorite from the answers list
+        messages = self._base_messages  # Local copy of messages to update with actual query
+        choice_dict = OrderedDict()     # Ordered dictionary of choices
+        choices = ''                    # Human-readable list formed from choice_dict
+        # Base prompt to prompt OpenAI to generate a single answer to a prompt
+        content = f'I am going to give you some answers for the Quiplash 3 prompt "{prompt}".  ' \
+                  + 'Pick the funniest answer from the choice list I give you.  ' \
+                  + f'Your comma-separated choice list is: {choices}.  ' \
+                  + 'Choose an answer from the choice list but only give me the letter.  ' \
+                  + 'Do not create new content.  Do not create any additional answers.  ' \
+                  + 'Do not create any new choices.  ' \
+                  + 'Do not choose a letter that was not in your choice list.'
 
         # CLASS VALIDATION
         self.setup()
 
+        # SETUP
+        for answer in answers:
+            choice_dict[chr(answers.index(answer) + 65)] = answer
+        choices = ', '.join([key + '. ' + val for (key, val) in choice_dict.items()])
+
         # VOTE IT
+        print(f'PROMPT: {prompt}')   # DEBUGGING
+        print(f'CHOICES: {choices}')  # DEBUGGING
+        print(f'CONTENT: {content}')  # DEBUGGING
+        messages.append({'role': 'user', 'content': content})
+        answer = self._create_content(messages=messages)
+        favorite = self._extract_favorite(answer, choice_dict)
 
         # DONE
         return favorite
@@ -106,3 +143,105 @@ class JitbAi:
         answer = completion.choices[0].message.content
         print(f"OPENAI'S ANSWER WAS {answer}")  # DEBUGGING
         return answer
+
+    def _extract_favorite(self, answer: str, choices: dict) -> str:
+        """Extract a favorite from created content.
+
+        Extract an answer from created content given a dictionary of available choices.  Will
+        randomize a selection from the dictionary values on any failure.
+
+        Args:
+            answer: Content created by OpenAI.
+            choices: A dictionary of available choices.  The keys represent shorthand
+                identification
+        """
+        # LOCAL VARIABLES
+        favorite = ''  # The favorite chosen from among the choices values
+
+        # INPUT VALIDATION
+        if self._failed_request(answer):
+            favorite = _randomize_choice(choices=choices)
+            print(f'OpenAI failed with {answer} so {favorite} was chosen randomly')  # DEBUGGING
+        # EXTRACT IT
+        elif answer in choices.keys():
+            favorite = choices[answer]  # Sometimes OpenAI follows instructions
+            print(f'OpenAI followed instructions and {favorite} was chosen')  # DEBUGGING
+        elif len(answer) != len(list(choices.keys())[0]) and answer[0] in choices.keys():
+            favorite = choices[answer[0]]  # Sometimes OpenAI likes to add words
+            print(f'OpenAI added words but we extracted {favorite} from {answer}')  # DEBUGGING
+        else:
+            # Did OpenAI give a response that wasn't a choice without failing the request?!
+            favorite = _randomize_choice(choices=choices)
+            print(f'OpenAI went crazy with {answer} so {favorite} was chosen randomly')  # DEBUGGING
+
+        # DONE
+        return favorite
+
+
+    def _failed_request(self, content: str) -> bool:
+        """Check OpenAI's response content for evidence of failure.
+
+        Args:
+            content: Response from OpenAI.
+
+        Returns:
+            True if there was any indication that OpenAI could not comply with the request.
+        """
+        # LOCAL VARIABLES
+        failed = False  # Found an indication of failure
+
+        # DID IT FAIL?
+        for fail_message in self._failure_content:
+            if _match_phrase(fail_message, content):
+                failed = True
+                break
+
+        # DONE
+        return failed
+
+
+def _match_phrase(needle: str, haystack: str, threshold: float = 0.75) -> bool:
+    """Loosely match a needle and a haystack to an established threshold.
+
+    If threshold percent, or more, of needle's words are found in haystack, then it is considered
+    a phrase match.
+
+    Args:
+        needle: The string to match in haystack.
+        haystack: The phrase to match against.
+        threshold: Optional; The percentage, as a float, to constitutes a phrase match.
+
+    Returns:
+        True if threshold percentage of needle words were found in the haystack, False otherwise.
+    """
+    # LOCAL VARIABLES
+    stripped_needle = ''  # A version of needle with all the punctuation and whitespace stripped out
+    word_count = 0        # Number of needle words
+    num_found = 0         # Number of needle words found in haystack
+    matched = False       # The threshold has been surpassed
+
+    # MATCH IT
+    # Strip needle of all punctuation and most whitespace
+    for char in needle:
+        if char in string.punctuation:
+            stripped_needle = stripped_needle + ' '
+        elif char in string.whitespace:
+            stripped_needle = stripped_needle + ' '
+        else:
+            stripped_needle = stripped_needle + char
+    # Start matching
+    for needle_word in stripped_needle.split(' '):
+        word_count += 1
+        if needle_word in haystack:
+            num_found += 1
+    # Check the threshold
+    if num_found / word_count >= threshold:
+        matched = True
+
+    # DONE
+    return matched
+
+
+def _randomize_choice(choices: dict) -> str:
+    """Randomize one of the values from choices."""
+    return random.choice(list(choices.values()))
