470e866b54766169834c2be3fce2649234585ad4
hark130 <hark130@yahoo.com>
2023-12-09 21:35:00 -0600

    JITB-1: Implemented new AI class into main()
    Changes to be committed:
    	modified:   jitb/jitb_main.py
    
    Changes not staged for commit:
    	modified:   jitb/jitb_website.py

--- jitb/jitb_main.py
+++ jitb/jitb_main.py
@@ -1,10 +1,12 @@
 """Defines the entry-point function for this package."""
 # Standard
+import os
 import sys
 # Third Party
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 # Local
 from jitb.jitb_args import parse_args
+from jitb.jitb_openai import JitbAi
 from jitb.jitb_website import play_the_game
 
 
@@ -18,16 +20,19 @@ def main() -> int:
     exit_code = 0   # 0 for success, 1 for failure.
     room_code = ''  # Jackbox Games room code
     username = ''   # Jackbox Games username
+    client = None   # JitbAi object
 
     # DO IT
     (room_code, username) = parse_args()
-    # print(f'ROOM CODE: {room_code}')  # DEBUGGING
-    # print(f'USERNAME: {username}')  # DEBUGGING
     try:
-        play_the_game(room_code=room_code, username=username)
+        client = JitbAi()
+        client.setup()
+        play_the_game(room_code=room_code, username=username, ai_obj=client)
     except (KeyboardInterrupt, NoSuchElementException, RuntimeError, TimeoutException) as err:
         _print_exception(err)
         exit_code = 1
+    finally:
+        client.tear_down()
 
     # DONE
     return exit_code
