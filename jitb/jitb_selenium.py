efb76e00d0b3907cbd730bb2104999481b99bb4c
hark130 <hark130@yahoo.com>
2024-02-28 20:54:03 -0600

    JITB-8: Implemented new selenium features
    - Implemented get_buttons()
    - Implemented get_sub_buttons()
    - Underpinned both with _get_elements()
    - Fixed a space-instead-of-underscore BUG in _validate_common_args()
    
    ----------------------------------------------------------------------
    Ran 167 tests in 213.162s
    
    OK (skipped=3)
    
    Changes to be committed:
    	modified:   jitb/jitb_selenium.py

--- jitb/jitb_selenium.py
+++ jitb/jitb_selenium.py
@@ -1,13 +1,82 @@
 """Common functionality for Selenium web drivers and web elements."""
 
 # Standard
-from typing import Any
+from typing import Any, Final, List
 # Third Party
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 from selenium.webdriver.common.by import By
 import selenium
 # Local
 from jitb.jitb_logger import Logger
+
+
+DEFAULT_BUTTON_BY: Final[str] = By.XPATH        # We find buttons by XPath, by default.
+DEFAULT_BUTTON_VALUE: Final[str] = './/button'  # XPath value to find buttons.
+
+
+def get_buttons(web_driver: selenium.webdriver.chrome.webdriver.WebDriver) \
+        -> List[selenium.webdriver.remote.webelement.WebElement]:
+    """Get a list of all the button type elements from web_driver starting at the root XPath.
+
+    Args:
+        web_driver: Selenium web driver to search for buttons.
+
+    Returns:
+        A list of all button WebElements on success.  An empty list if none were found.
+    """
+    # LOCAL VARIABLES
+    button_by = DEFAULT_BUTTON_BY        # Use the XPath to find all the button web elements
+    button_value = DEFAULT_BUTTON_VALUE  # XPath value to find all the buttons
+    element_list = []                    # List of all the button web elements
+
+    # INPUT VALIDATION
+    _validate_wd_input(web_driver=web_driver, by_arg=button_by, value=button_value)
+
+    # GET IT
+    # print(f'get_buttons() has {web_driver} {button_by} {button_value}')  # DEBUGGING
+    element_list = _get_elements(web_thing=web_driver, by_arg=button_by, value=button_value)
+
+    # DONE
+    return element_list
+
+
+def get_sub_buttons(web_driver: selenium.webdriver.chrome.webdriver.WebDriver,
+                    sub_by: str = By.ID, sub_value: str = None) \
+        -> List[selenium.webdriver.remote.webelement.WebElement]:
+    """Get a list of all the button type elements from web_driver starting at a web element.
+
+    Some of these web pages have hidden and/or disabled buttons hidden on the page.
+    This function will first get the sub-web element indicated by sub_by and sub_value.  Then,
+    it will start searching for the button type elements there.  That way, you can better target
+    the buttons you're looking for.
+
+    Args:
+        web_driver: Selenium web driver to search for buttons.
+        sub_by: Optional; See: help(selenium.webdriver.common.by.By).
+        sub_value: Optional; Value of the by_arg-type of web element.
+
+    Returns:
+        A list of the button WebElements found at the sub-web element on success.
+        An empty list if none were found.
+    """
+    # LOCAL VARIABLES
+    button_by = DEFAULT_BUTTON_BY        # Use the XPath to find all the button web elements
+    button_value = DEFAULT_BUTTON_VALUE  # XPath value to find all the buttons
+    sub_element = None                   # Element found with sub_by and sub_value
+    element_list = []                    # List of all the button web elements
+
+    # INPUT VALIDATION
+    _validate_wd_input(web_driver=web_driver, by_arg=sub_by, value=sub_value)
+
+    # GET IT
+    # Get the sub web element
+    sub_element = _get_element(web_thing=web_driver, by_arg=sub_by, value=sub_value)
+    # Get the buttons
+    if sub_element:
+        element_list = _get_elements(web_thing=sub_element, by_arg=button_by, value=button_value)
+
+    # DONE
+    return element_list
 
 
 def get_sub_element(web_element: selenium.webdriver.remote.webelement.WebElement,
@@ -146,13 +215,50 @@ def _get_element(web_thing: Any, by_arg: str = By.ID, value: str = None) \
     return element
 
 
+def _get_elements(web_thing: Any, by_arg: str = By.ID, value: str = None) \
+        -> List[selenium.webdriver.remote.webelement.WebElement]:
+    """Find an element, in web_thing, if the find_elements method exists.
+
+    Does not validate input other than verifying web_thing.find_elements() exists.
+
+    Args:
+        web_thing: Basically, any object that has a find_element method.  This private function
+            was written with the Selenium WebDriver and WebElement in mind.
+        by_arg: Optional; See: help(selenium.webdriver.common.by.By).
+        value: Optional; Value of the by_arg-type of web element.
+
+    Returns:
+        A list of any WebElements found, empty list otherwise.
+    """
+    # LOCAL VARIABLES
+    elements = []                # Found selenium.webdriver.remote.webelement.WebElement
+    attr_name = 'find_elements'  # The attribute in question
+    get_it = None                # Found attribute
+
+    # INPUT VALIDATION
+    if web_thing and hasattr(web_thing, attr_name):
+        get_it = getattr(web_thing, attr_name)
+        if callable(get_it):
+            try:
+                # print(f'_get_elements({by_arg}, {value})')  # DEBUGGING
+                elements = get_it(by=by_arg, value=value)
+                # print(f'get_it() returned {elements}')  # DEBUGGING
+            except (NoSuchElementException, StaleElementReferenceException) as err:
+                Logger.debug(f'Getting elements {by_arg}:{value} raised {repr(err)}!')
+    else:
+        Logger.debug(f'_get_elements reports the web thing had no attr {attr_name}!')
+
+    # DONE
+    return elements
+
+
 def _validate_common_args(by_arg: str, value: str) -> None:
     """Validate the cross-section of this module's API functions."""
     # INPUT VALIDATION
     # by_arg
     if not isinstance(by_arg, str):
         raise TypeError(f'Invalid data type of {type(by_arg)} for the by_arg')
-    if not hasattr(By, by_arg.upper()):
+    if not hasattr(By, by_arg.replace(' ', '_').upper()):
         raise ValueError(f'Invalid by_arg value of {by_arg}.  Use By value from '
                          'the selenium.webdriver.common.by module')
     # value
