3f8468c861151c9983e0c18b0368e45bd7a79c48
hark130 <hark130@yahoo.com>
2024-09-24 23:55:58 -0500

    JITB-25: Refactored jitb's entry point
    Now there's an actual function for the pip installer to utilize.
    
    Changes to be committed:
    	modified:   jitb/__main__.py
    
    Untracked files:
    	pyproject.toml
    	requirements.txt

--- jitb/__main__.py
+++ jitb/__main__.py
@@ -1,4 +1,5 @@
 """Entry level module for the package."""
+
 # Standard
 import sys
 # Third Party
@@ -6,5 +7,10 @@ import sys
 from jitb.jitb_main import main
 
 
+def run_jitb() -> int:
+    """Wraps the call to main() translating results into exit codes and exits."""
+    return main()
+
+
 if __name__ == '__main__':
-    sys.exit(main())
+    sys.exit(run_jitb())
