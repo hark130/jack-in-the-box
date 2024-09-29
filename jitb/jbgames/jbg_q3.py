1a5c423df0dc080d3b22234eded3a97976270d26
hark130 <hark130@yahoo.com>
2024-09-29 02:06:26 -0500

    JITB-32: Final(?) Quiplash 3 update
    - Improved code quality by increasing extra-package function use
    - Made linters happy
    - Fixed a Thriplash BUG (it was using the wrong clues)
    - Removed all the old code
    
    It appears to be completely successful during manual system testing.
    
    Changes to be committed:
    	modified:   jitb/jbgames/jbg_q3.py
    
    Changes not staged for commit:
    	modified:   jitb/jitb_main.py (still shunted)

--- jitb/jbgames/jbg_q3.py
+++ jitb/jbgames/jbg_q3.py
@@ -19,7 +19,7 @@ from jitb.jitb_logger import Logger
 from jitb.jitb_openai import JitbAi
 from jitb.jitb_selenium import get_buttons, get_web_element
 from jitb.jitb_webdriver import (click_a_button, get_button_choices, get_prompt, get_vote_text,
-                                 is_prompt_page, is_vote_page, write_an_answer)
+                                 is_prompt_page, is_vote_page, vote_answers, write_an_answer)
 
 
 DEFAULT_CHAR_LIMIT: Final[int] = 45  # Default maximum character limit
@@ -120,7 +120,7 @@ class JbgQ3(JbgAbc):
                 Logger.debug('Tried to click the select character button '
                              f'{button_entry.accessible_name} but caught {repr(err)}')
                 button_list.remove(button_entry)  # Bad button; remove it and keep trying
-            except StaleElementReferenceException as err:
+            except StaleElementReferenceException:
                 Logger.debug('Tried to click the select character button '
                              f'{button_entry.accessible_name} but caught a '
                              'StaleElementReferenceException exception.  Is this a race '
@@ -171,9 +171,10 @@ class JbgQ3(JbgAbc):
 
         # VOTE IT
         while True:
-            if not self.is_vote_page(web_driver):
-                break  # Must have already answered them all
-            prompt_text = self.vote_answer(web_driver=web_driver, last_prompt=prompt_text)
+            prompt_text = vote_answers(web_driver=web_driver, last_prompt=prompt_text,
+                                       ai_obj=self._ai_obj, element_name='prompt',
+                                       element_type=By.ID, vote_clues=self._vote_clues,
+                                       clean_string=False)
             if not prompt_text:
                 break
 
@@ -229,7 +230,7 @@ class JbgQ3(JbgAbc):
             raise RuntimeError('This is not the Thriplash prompt page')
 
         # ANSWER THRIPLASH
-        prompt_text = self.get_prompt(web_driver=web_driver, prompt_clues=self._prompt_clues)[-1]
+        prompt_text = self.get_prompt(web_driver=web_driver, prompt_clues=self._thrip_clues)[-1]
         gen_answers = self._ai_obj.generate_thriplash(prompt_text)
         temp_answers = gen_answers[::-1]  # Reverse it so they can be pop()d
         input_fields = web_driver.find_elements(By.ID, 'input-text-textarea')
@@ -324,62 +325,6 @@ class JbgQ3(JbgAbc):
         self._check_web_driver(web_driver=web_driver)
         self._validate_core_attributes()
 
-    def vote_answer(self, web_driver: selenium.webdriver.chrome.webdriver.WebDriver,
-                    last_prompt: str) -> str:
-        """Generate votes for other players prompts.
-
-        Args:
-            web_driver: The web driver to interact with.
-            last_prompt: The last prompt that was voted.  Helps this function avoid trying to
-                vote the same prompt twice.
-
-        Returns:
-            The prompt that was voted.
-
-        Raises:
-            RuntimeError: Did not vote an answer.
-        """
-        # LOCAL VARIABLES
-        prompt_text = ''    # Input prompt
-        button = None       # The web element of the button to click
-        clicked_it = False  # Keep track of whether this prompt was answered or not
-        num_loops = 5       # Number of attempts to make for a new prompt
-        choice_list = []    # List of possible answers
-        favorite = ''       # OpenAI's favorite answer
-        button_dict = {}    # Sanitized text are the keys and actual button text are the values
-
-        # WAIT FOR IT
-        for _ in range(num_loops):
-            try:
-                prompt_text = self.get_vote_text(web_driver, vote_clues=self._vote_clues)
-                if prompt_text and prompt_text != last_prompt:
-                    break
-                if not self.is_vote_page(web_driver):
-                    prompt_text = ''
-                    break
-                time.sleep(JITB_POLL_RATE)
-            except NoSuchElementException:
-                prompt_text = ''  # Must have been the last prompt to vote
-
-        # ANSWER IT
-        if prompt_text and prompt_text != last_prompt:
-            button_dict = get_button_choices(web_driver=web_driver)
-            if button_dict:
-                choice_list = [button for button, _ in button_dict.items() if button]
-                # Ask the AI
-                favorite = self._ai_obj.vote_favorite(prompt=prompt_text, answers=choice_list)
-                # Click it
-                clicked_it = click_a_button(web_driver=web_driver, button_str=button_dict[favorite])
-        else:
-            prompt_text = ''  # Nothing got answered
-
-        # DONE
-        if prompt_text and prompt_text != last_prompt and not clicked_it:
-            raise RuntimeError('Did not vote an answer')
-        if button:
-            Logger.debug(f'VOTED {favorite} for {prompt_text}!')
-        return prompt_text
-
     def submit_an_answer(self, web_driver: selenium.webdriver.chrome.webdriver.WebDriver,
                          submit_text: str) -> bool:
         """Write an answer and click the submit button.
@@ -457,218 +402,3 @@ class JbgQ3(JbgAbc):
             raise RuntimeError('Did not answer the prompt')
         Logger.debug(f'Answered prompt "{prompt_text}" with "{answer}"!')
         return prompt_text
-
-
-# Private Functions (alphabetical order)
-# def _answer_thriplash(web_driver: selenium.webdriver.chrome.webdriver.WebDriver,
-#                       ai_obj: JitbAi) -> None:
-#     """Generate answers for the Thriplash (Round 3) prompt."""
-#     # LOCAL VARIABLES
-#     prompt_text = ''    # Input prompt
-#     input_fields = []   # List of web elements for the three input fields
-#     buttons = []        # List of button web elements
-#     clicked_it = False  # Keep track of whether this prompt was answered or not
-#     gen_answers = []    # Answers generated by OpenAI
-#     temp_answers = []   # Temp list to reverse and then pop from
-
-#     # INPUT VALIDATION
-#     if not self.is_thrip_prompt_page(web_driver):
-#         raise RuntimeError('This is not the Thriplash prompt page')
-
-#     # ANSWER IT
-#     prompt_text = _get_prompt(web_driver=web_driver)[-1]
-#     gen_answers = ai_obj.generate_thriplash(prompt_text)
-#     temp_answers = gen_answers[::-1]  # Reverse it so they can be pop()d
-#     input_fields = web_driver.find_elements(By.ID, 'input-text-textarea')
-
-#     # SUBMIT IT
-#     for input_field in input_fields:
-#         input_field.send_keys(temp_answers.pop())
-#     buttons = web_driver.find_elements(By.XPATH, '//button')
-#     for button in buttons:
-#         if button.text.lower() == 'SUBMIT'.lower() and button.is_enabled():
-#             button.click()
-#             clicked_it = True
-#             break
-
-#     # DONE
-#     if not clicked_it:
-#         raise RuntimeError('Did not answer the Thriplash prompt')
-#     Logger.debug(f'ANSWERED THRIPLASH {prompt_text} with: {", ".join(gen_answers)}!')
-
-
-# def _get_prompt(web_driver: selenium.webdriver.chrome.webdriver.WebDriver) -> list:
-#     """Wait for the prompt element id to show and then return.
-
-#     Returns:
-#         The game prompt, split by newline, into a list.
-#     """
-#     # LOCAL VARIABLES
-#     prompt_text = []  # The prompt's text split by newline
-
-#     # WAIT FOR IT
-#     element = web_driver.find_element(By.ID, 'prompt')
-#     prompt_text = element.text.split('\n')
-
-#     # DONE
-#     if not prompt_text:
-#         raise RuntimeError('Did not detect any prompts')
-#     return prompt_text
-
-
-# def _is_char_selection_page(web_driver: selenium.webdriver.chrome.webdriver.WebDriver) -> bool:
-#     """Determine if this is this the character selection screen.
-
-#     Returns:
-#         True if this is the 'Select your character' screen, False otherwise.
-#     """
-#     # LOCAL VARIABLES
-#     char_selection = False  # Prove this true
-#     prompt = None           # Web Element for the characters prompt
-
-#     # IS IT?
-#     try:
-#         prompt = web_driver.find_element(By.ID, 'charactersPrompt')
-#         if 'Select your character'.lower() in prompt.text.lower():
-#             char_selection = True
-#     except (NoSuchElementException, StaleElementReferenceException, TypeError, ValueError):
-#         pass  # Not the character selection page
-
-#     # DONE
-#     return char_selection
-
-
-# def _is_prompt_page(web_driver: selenium.webdriver.chrome.webdriver.WebDriver) -> bool:
-#     """Determine if this is this a round 1 or 2 prompt page.
-
-#     Returns:
-#         True if this is a regular prompt screen, False otherwise.
-#     """
-#     # LOCAL VARIABLES
-#     prompt_page = False                           # Prove this true
-#     temp_we = None                                # Temporary web element variable
-#     prompts = ['Prompt 1 of 2', 'Prompt 2 of 2']  # Prompt needles
-
-#     # IS IT?
-#     try:
-#         temp_we = web_driver.find_element(By.ID, 'prompt')
-#         for prompt in prompts:
-#             if temp_we.text.startswith(prompt):
-#                 prompt_page = True  # If we made it here, it's a prompt page
-#                 break
-#     except (NoSuchElementException, StaleElementReferenceException, TypeError, ValueError):
-#         pass  # Not a prompt page
-
-#     # DONE
-#     return prompt_page
-
-
-# def _is_thrip_prompt_page(web_driver: selenium.webdriver.chrome.webdriver.WebDriver) -> bool:
-#     """Determine if this is the Thriplash (Round 3) prompt page.
-
-#     Returns:
-#         True if this is the Thriplash (Round 3) prompt page, False otherwise.
-#     """
-#     # LOCAL VARIABLES
-#     prompt_page = False      # Prove this true
-#     temp_we = None           # Temporary web element variable
-#     prompt = 'Final prompt'  # Prompt needle
-
-#     # IS IT?
-#     try:
-#         temp_we = web_driver.find_element(By.ID, 'prompt')
-#         if temp_we.text.lower().startswith(prompt.lower()):
-#             prompt_page = True  # If we made it here, it's a prompt page
-#     except (NoSuchElementException, StaleElementReferenceException, TypeError, ValueError):
-#         pass  # Not a Thriplash prompt page
-
-#     # DONE
-#     return prompt_page
-
-
-# def _is_vote_page(web_driver: selenium.webdriver.chrome.webdriver.WebDriver) -> bool:
-#     """Determine if this is this a round 1 or 2 vote page.
-
-#     Returns:
-#         True if this is a regular vote screen, False otherwise.
-#     """
-#     # LOCAL VARIABLES
-#     vote_page = False                  # Prove this true
-#     temp_we = None                     # Temporary web element variable
-#     prompt = 'Vote for your favorite'  # Prompt needles
-
-#     # IS IT?
-#     try:
-#         temp_we = web_driver.find_element(By.ID, 'prompt')
-#         if prompt.lower() in temp_we.text.lower():
-#             vote_page = True  # If we made it here, it's a vote page
-#     except (NoSuchElementException, StaleElementReferenceException, TypeError, ValueError):
-#         pass  # Not a vote page
-
-#     # DONE
-#     return vote_page
-
-
-# def _vote_answer(web_driver: selenium.webdriver.chrome.webdriver.WebDriver,
-#                  last_prompt: str, ai_obj: JitbAi) -> str:
-#     """Generate votes for other players prompts.
-
-#     Args:
-#         last_prompt: The last prompt that was answered.  Helps this function avoid trying to
-#             answer the same prompt twice.
-
-#     Returns:
-#         The prompt that was answered as a string.
-
-#     Raises:
-#         RuntimeError: The prompt wasn't answered.
-#     """
-#     # LOCAL VARIABLES
-#     prompt_text = ''    # Input prompt
-#     buttons = []        # List of web elements for the buttons
-#     button = None       # The web element of the button to click
-#     clicked_it = False  # Keep track of whether this prompt was answered or not
-#     num_loops = 5       # Number of attempts to make for a new prompt
-#     choice_list = []    # List of possible answers
-#     favorite = ''       # OpenAI's favorite answer
-#     button_dict = {}    # Sanitized text are the keys and actual button text are the values
-#     temp_text = ''      # Temp sanitized button text
-
-#     # WAIT FOR IT
-#     for _ in range(num_loops):
-#         try:
-#             prompt_text = _get_prompt(web_driver)[0]
-#             if prompt_text and prompt_text != last_prompt:
-#                 break
-#             if not _is_vote_page(web_driver):
-#                 prompt_text = ''
-#                 break
-#             time.sleep(JITB_POLL_RATE)
-#         except NoSuchElementException:
-#             prompt_text = ''  # Must have been the last prompt to vote
-
-#     # ANSWER IT
-#     if prompt_text and prompt_text != last_prompt:
-#         buttons = web_driver.find_elements(By.XPATH, '//button')
-#         # Form the selection list
-#         for button in buttons:
-#             if button.text:
-#                 temp_text = button.text.strip('\n')
-#                 button_dict[temp_text] = button.text
-#                 choice_list.append(temp_text)
-#         # Ask the AI
-#         favorite = ai_obj.vote_favorite(prompt=prompt_text, answers=choice_list)
-#         for button in buttons:
-#             if button and button.text == button_dict[favorite] and button.is_enabled():
-#                 button.click()
-#                 clicked_it = True
-#                 break
-#     else:
-#         prompt_text = ''  # Nothing got answered
-
-#     # DONE
-#     if prompt_text and prompt_text != last_prompt and not clicked_it:
-#         raise RuntimeError('Did not vote an answer')
-#     if button:
-#         Logger.debug(f'VOTED {favorite} for {prompt_text}!')
-#     return prompt_text
