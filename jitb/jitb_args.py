e1fbcaf89e98460bb7a8f71891227f49a077b6ae
hark130 <hark130@yahoo.com>
2024-12-20 18:29:25 -0600

    JITB-42: Moderate parse args refactor
    The codebase still needs to know the room code (to help validate usage)
    so I refactored that to be a mandatory argument for both commands.
    
    I also cleaned up(?) the parse_args() function a bit.
    
    Changes to be committed:
    	modified:   jitb/jitb_args.py
    
    Changes not staged for commit:
    	modified:   jitb/jitb_main.py
    	modified:   jitb/jitb_website.py
    
    Untracked files:
    	test/test_input/JackboxTV-Q3-Login-Twitch_required-error.html
    	test/test_input/JackboxTV-Q3-Login-Twitch_required-error_files/
    	test/test_input/JackboxTV-Q3-Login-Twitch_required-login.html
    	test/test_input/JackboxTV-Q3-Login-Twitch_required-login_files/

--- jitb/jitb_args.py
+++ jitb/jitb_args.py
@@ -1,5 +1,6 @@
 """Parse the command line arguments on behalf of the package."""
 # Standard
+from typing import Any
 import argparse
 import os
 # Third Party
@@ -16,9 +17,22 @@ def parse_args() -> ArgVals:
     Returns:
         A ArgVals data class containing all parsed values.
     """
+    # LOCAL VARIABLES
+    room_arg_name = 'room'                          # The proper name of the room code argument
+    user_arg_name = 'user'                          # The proper name of the username argument
+    room_code = None                                # Parsed room value (may be None)
+    username = None                                 # Parsed username (may be None)
     jitb_games = list(JITB_SUPPORTED_GAMES.keys())  # JITB supported games
-    jitb_games.sort()
+    parser = None                                   # ArgumentParser object
+    subparsers = None                               # Subparsers
+    manual_parser = None                            # The 'manual' command subparser
+    auto_parser = None                              # The 'automatic' command subparser
+    args = None                                     # Parsed argument Namespace
+    # Debug log location
     debug_log = os.path.join(determine_tmp_dir(), 'jitb_YYYYMMDD_HHMMSS-#.log')
+
+    # SETUP
+    jitb_games.sort()
     parser = argparse.ArgumentParser(prog='jitb',
                                      description='Jack in the Box (JITB): Connecting Jackbox '
                                                  'Games to the OpenAI API.  JITB currently '
@@ -27,17 +41,38 @@ def parse_args() -> ArgVals:
     manual_parser = subparsers.add_parser(JITB_ARG_CMDS_MAN[0], aliases=JITB_ARG_CMDS_MAN[1:],
                                           help='Human interaction is required to login '
                                           '(e.g., Twitch-enabled login)')
+    manual_parser.add_argument(f'-{room_arg_name[0]}', f'--{room_arg_name}', action='store',
+                               help='The Jackbox Games room code', required=True)
     auto_parser = subparsers.add_parser(JITB_ARG_CMDS_AUTO[0], aliases=JITB_ARG_CMDS_AUTO[1:],
                                         help='JITB will automatically login')
-    auto_parser.add_argument('-r', '--room', action='store', help='The Jackbox Games room code',
-                             required=True)
-    auto_parser.add_argument('-u', '--user', action='store', help='The Jackbox Games name',
-                             required=True)
+    auto_parser.add_argument(f'-{room_arg_name[0]}', f'--{room_arg_name}', action='store',
+                             help='The Jackbox Games room code', required=True)
+    auto_parser.add_argument(f'-{user_arg_name[0]}', f'--{user_arg_name}', action='store',
+                             help='The Jackbox Games username', required=True)
     parser.add_argument('-d', '--debug', action='store_true',
                         help=f'Log debug messages to {debug_log} (Change the dir with '
                              f'the {TEMP_DIR_ENV_VARS[0]} environment variable)',
                         required=False)
+
+    # PARSE IT
     args = parser.parse_args()
+    room_code = _get_eafp_attr(args, room_arg_name)  # Get the room code
+    username = _get_eafp_attr(args, user_arg_name)  # Get the username
 
     # DONE
-    return ArgVals(args.command, args.debug, room_code=args.room, username=args.user)
+    return ArgVals(args.command, args.debug, room_code=room_code, username=username)
+
+
+def _get_eafp_attr(args: argparse.Namespace, attr: str) -> Any:
+    """Safely retrieve values from a Namespace (if they exist)."""
+    # LOCAL VARIABLES
+    value = None  # Retrieved value
+
+    # GET IT
+    try:
+        value = getattr(args, attr)
+    except AttributeError:
+        pass  # Easier to ask for forgiveness than permission (EAFP)
+
+    # DONE
+    return value
