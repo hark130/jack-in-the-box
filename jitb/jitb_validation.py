a5447c2faa6d2f0b150c8677b87e5426e8a3ada6
hark130 <hark130@yahoo.com>
2024-12-20 23:23:28 -0600

    JITB-40: Added website validation
    Changes to be committed:
    	modified:   jitb/jitb_validation.py
    
    Changes not staged for commit:
    	modified:   jitb/jitb_main.py
    	modified:   jitb/jitb_website.py

--- jitb/jitb_validation.py
+++ jitb/jitb_validation.py
@@ -1,7 +1,9 @@
 """Defines standard validation for the package."""
 
 # Standard
+from typing import Any, Dict
 # Third Party
+from hobo.validation import validate_type
 import selenium
 # Local
 
@@ -20,6 +22,29 @@ def validate_bool(value: str, name: str) -> None:
 def validate_element_type(element_type: str) -> None:
     """Validate element_type arguments on behalf of this module."""
     validate_string(element_type, 'element_type', may_be_empty=False)
+
+
+def validate_game(game: str, games: Dict[str, Any]) -> None:
+    """Validate the game against the dictionary of support games.
+
+    Why this odd validation?  Why is the value type hint Any?  To avoid circular
+    imports.  The expected value type is actually JbgAbc, but that doesn't
+    matter here.
+
+    Args:
+        game: The game to be checked for support.
+        games: A dictionary of games JITB supports.  Only the keys are inspected.
+
+    Raises:
+        RuntimeError: This game is not supported.
+    """
+    # INPUT VALIDATION
+    validate_string(game, 'game')
+    validate_type(games, 'games', dict)
+
+    # VALIDATE GAME
+    if game not in list(games.keys()):
+        raise RuntimeError(f'JITB does not yet support {game}')
 
 
 def validate_string(string: str, name: str, may_be_empty: bool = False) -> None:
