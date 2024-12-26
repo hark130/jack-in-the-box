38d0010eb91c6e37287377b9323c5e5df847d32b
hark130 <hark130@yahoo.com>
2024-12-25 23:11:56 -0600

    JITB-15: Updated some JbgBr unit tests
    Updated to match a prompt engineering refactor.
    
    ----------------------------------------------------------------------
    Ran 620 tests in 701.010s
    
    OK (skipped=7)
    
    Changes to be committed:
    	modified:   test/unit_test/test_jbgbr/test_get_guess_prompt.py

--- test/unit_test/test_jbgbr/test_get_guess_prompt.py
+++ test/unit_test/test_jbgbr/test_get_guess_prompt.py
@@ -93,84 +93,113 @@ class NormalTestJbgBrGetGuessPrompt(TestJbgBrGetGuessPrompt):
     def test_n01_round_1_guess_1a(self):
         """Blather 'Round Round 1 Guess 1a."""
         self.create_br_input('JackboxTV-BR-5a-Guess.html')
-        self.expect_return("Magugol is presenting a: story. It's a story about a dull good guy. "
-                           "What story is magugol describing?")
+        self.expect_return("The player Magugol is giving clues about the name of a well-known "
+                           "story which could be a book, a movie, etc. "
+                           "It's a story about a dull good guy. "
+                           "Answer the following in one or two words without adding "
+                           "adjectives or adverbs... What story is magugol describing?")
         self.run_test()
 
     def test_n02_round_1_guess_1b(self):
         """Blather 'Round Round 1 Guess 1b."""
         self.create_br_input('JackboxTV-BR-5b-Guess.html')
-        self.expect_return("Magugol is presenting a: story. It's a story about a dull good guy. "
+        self.expect_return("The player Magugol is giving clues about the name of a well-known "
+                           "story which could be a book, a movie, etc. "
+                           "It's a story about a dull good guy. "
                            "And a fantastic experience. "
-                           "What story is magugol describing?")
+                           "Answer the following in one or two words without adding "
+                           "adjectives or adverbs... What story is magugol describing?")
         self.run_test()
 
     def test_n03_round_1_guess_1c(self):
         """Blather 'Round Round 1 Guess 1c."""
         self.create_br_input('JackboxTV-BR-5c-Guess.html')
-        self.expect_return("Magugol is presenting a: story. It's a story about a dull good guy. "
+        self.expect_return("The player Magugol is giving clues about the name of a well-known "
+                           "story which could be a book, a movie, etc. "
+                           "It's a story about a dull good guy. "
                            "And a fantastic experience. "
                            "Whoa! A perky paddle. "
-                           "What story is magugol describing?")
+                           "Answer the following in one or two words without adding "
+                           "adjectives or adverbs... What story is magugol describing?")
         self.run_test()
 
     def test_n04_round_1_guess_1d(self):
         """Blather 'Round Round 1 Guess 1d."""
         self.create_br_input('JackboxTV-BR-5d-Guess.html')
-        self.expect_return("Magugol is presenting a: story. It's a story about a dull good guy. "
+        self.expect_return("The player Magugol is giving clues about the name of a well-known "
+                           "story which could be a book, a movie, etc. "
+                           "It's a story about a dull good guy. "
                            "And a fantastic experience. "
                            "Whoa! A perky paddle. "
                            "The hero adores the human. "
-                           "What story is magugol describing?")
+                           "Answer the following in one or two words without adding "
+                           "adjectives or adverbs... What story is magugol describing?")
         self.run_test()
 
     def test_n05_round_1_guess_2a(self):
         """Blather 'Round Round 1 Guess 2a."""
         self.create_br_input('JackboxTV-BR-6a-Guess_Place.html')
-        self.expect_return("Mumu is presenting a: place. It's a vibrant place. "
-                           "What place is mumu describing?")
+        self.expect_return("The player Mumu is giving clues about a specific, well-known "
+                           "place, real or fictional, that exists in media or the real world. "
+                           "It's a vibrant place. "
+                           "Answer the following in one or two words without adding adjectives "
+                           "or adverbs... What place is mumu describing?")
         self.run_test()
 
     def test_n06_round_1_guess_2b(self):
         """Blather 'Round Round 1 Guess 2b."""
         self.create_br_input('JackboxTV-BR-6b-Guess_Place.html')
-        self.expect_return("Mumu is presenting a: place. It's a vibrant place. "
+        self.expect_return("The player Mumu is giving clues about a specific, well-known "
+                           "place, real or fictional, that exists in media or the real world. "
+                           "It's a vibrant place. "
                            "It's where you have the brand. "
-                           "What place is mumu describing?")
+                           "Answer the following in one or two words without adding adjectives "
+                           "or adverbs... What place is mumu describing?")
         self.run_test()
 
     def test_n07_round_1_guess_2c(self):
         """Blather 'Round Round 1 Guess 2c."""
         self.create_br_input('JackboxTV-BR-6c-Guess_Place-guessed_wrong.html')
-        self.expect_return("Mumu is presenting a: place. It's a vibrant place. "
+        self.expect_return("The player Mumu is giving clues about a specific, well-known "
+                           "place, real or fictional, that exists in media or the real world. "
+                           "It's a vibrant place. "
                            "It's where you have the brand. "
-                           "What place is mumu describing?")
+                           "Answer the following in one or two words without adding adjectives "
+                           "or adverbs... What place is mumu describing?")
         self.run_test()
 
     def test_n08_round_1_guess_2d(self):
         """Blather 'Round Round 1 Guess 2d."""
         self.create_br_input('JackboxTV-BR-6d-Guess_Place.html')
-        self.expect_return("Mumu is presenting a: place. It's a vibrant place. "
+        self.expect_return("The player Mumu is giving clues about a specific, well-known "
+                           "place, real or fictional, that exists in media or the real world. "
+                           "It's a vibrant place. "
                            "It's where you have the brand. "
                            "So much rubber! "
-                           "What place is mumu describing?")
+                           "Answer the following in one or two words without adding adjectives "
+                           "or adverbs... What place is mumu describing?")
         self.run_test()
 
     def test_n09_round_1_guess_2e(self):
         """Blather 'Round Round 1 Guess 2e."""
         self.create_br_input('JackboxTV-BR-6e-Guess_Place-guessed_wrong.html')
-        self.expect_return("Mumu is presenting a: place. It's a vibrant place. "
+        self.expect_return("The player Mumu is giving clues about a specific, well-known "
+                           "place, real or fictional, that exists in media or the real world. "
+                           "It's a vibrant place. "
                            "It's where you have the brand. "
                            "So much rubber! "
-                           "What place is mumu describing?")
+                           "Answer the following in one or two words without adding adjectives "
+                           "or adverbs... What place is mumu describing?")
         self.run_test()
 
     def test_n10_round_2_guess_4b(self):
         """Blather 'Round Round 2 Guess 4b."""
         self.create_br_input('JackboxTV-BR-Round_2-4b-Guess_person.html')
-        self.expect_return("Mumu is presenting a: person. They're a fictional  entity. "
-                           "They are renowned for the patience. "
-                           "What person is mumu describing?")
+        self.expect_return("The player Mumu is giving clues about a specific unique person "
+                           "who could be fictional, real, pop-culture icon, alien, etc. "
+                           "They're a fictional  entity. They are renowned for the patience. "
+                           "Answer the following in one or two words without adding "
+                           "adjectives or adverbs... What person is mumu describing?")
         self.run_test()
 
 
@@ -202,79 +231,79 @@ class SpecialTestJbgBrGetGuessPrompt(TestJbgBrGetGuessPrompt):
     def test_s01_non_jackbox_game_page(self):
         """Non-Jackbox Games website."""
         self.create_br_input('xkcd-Good_Code.html')
-        self.expect_page_exception()
+        self.expect_br_page_exception()
         self.run_test()
 
     def test_s02_login_page(self):
         """Jackbox.tv Login page."""
         self.create_br_input('JackboxTV-JB-Login_start.html')
-        self.expect_page_exception()
+        self.expect_br_page_exception()
         self.run_test()
 
     def test_s03_joke_boat_prompt_is_not_a_blather_round_prompt(self):
         """Joke Boat Round 1 Joke 1c complete punchline page."""
         self.create_br_input('JackboxTV-JB-Round_1-Joke_1-C_Complete_your_punchline.html')
-        self.expect_page_exception()
+        self.expect_br_page_exception()
         self.run_test()
 
     def test_s04_login_page(self):
         """Jackbox.tv Login page."""
         self.create_br_input('JackboxTV-BR-1a-Login_Page-pre_login.html')
-        self.expect_page_exception()
+        self.expect_br_page_exception()
         self.run_test()
 
     def test_s05_waiting_on_other_players(self):
         """Blather 'Round waiting to start."""
         self.create_br_input('JackboxTV-BR-1b-Login_Page-waiting.html')
-        self.expect_page_exception()
+        self.expect_br_page_exception()
         self.run_test()
 
     def test_s06_round_1_waiting(self):
         """Blather 'Round Chose secret prompt; waiting."""
         self.create_br_input('JackboxTV-BR-2b-Choose_Prompt-waiting.html')
-        self.expect_page_exception()
+        self.expect_br_page_exception()
         self.run_test()
 
     def test_s07_round_1_guess_2f(self):
         """Blather 'Round Round 1 Guess 2f."""
         self.create_br_input('JackboxTV-BR-6f-Guess_Place-guessed_right-waiting.html')
-        self.expect_page_exception()
+        self.expect_br_page_exception()
         self.run_test()
 
     def test_s08_round_1_make_a_sentence(self):
         """Blather 'Round Round 1 make a sentence."""
         self.create_br_input('JackboxTV-BR-3-Make_Sentence.html')
-        self.expect_page_exception()
+        self.expect_br_page_exception()
         self.run_test()
 
     def test_s09_round_1_describe(self):
         """Blather 'Round Round 1 describe."""
         self.create_br_input('JackboxTV-BR-4a-Describe.html')
-        self.expect_page_exception()
+        self.expect_br_page_exception()
         self.run_test()
 
     def test_s10_round_2_make_a_sentence(self):
         """Blather 'Round Round 2 make a sentence."""
         self.create_br_input('JackboxTV-BR-Round_2-2-Make_Sentence.html')
-        self.expect_page_exception()
+        self.expect_br_page_exception()
         self.run_test()
 
     def test_s11_round_2_describe(self):
         """Blather 'Round Round 2 describe."""
         self.create_br_input('JackboxTV-BR-Round_2-3b-Describe.html')
-        self.expect_page_exception()
+        self.expect_br_page_exception()
         self.run_test()
 
     def test_s12_game_done_waiting(self):
         """Blather 'Round game done; waiting page."""
         self.create_br_input('JackboxTV-Dict-End_of_game.html')
-        self.expect_page_exception()
+        self.expect_br_page_exception()
         self.run_test()
 
     def test_s13_game_over_disconnected(self):
         """Blather 'Round game over page; disconnected."""
         self.create_test_input('JackboxTV-Dict-Disconnected.html')
-        self.expect_page_exception()
+        self.expect_br_page_exception()
         self.run_test()
 # pylint: enable = too-many-public-methods
 
